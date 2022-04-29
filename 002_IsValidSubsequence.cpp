7559dd2f04c7e24bbd427269d97e4b5d5d1ef6e0
Hyeonsong Kim <83186385+kimhxsong@users.noreply.github.com>
2022-04-29 23:55:39 +0900

    Solve IsValidSubsequence; #Array #Easy

---
+++ 002_IsValidSubsequence.cpp
@@ -0,0 +1,32 @@
+// Copyright 2022 by @kimhxsong
+// Difficulty: #Easy
+// Category: #Array
+
+using namespace std;
+
+// Sol-1 T=O(n), S=O(1)
+bool isValidSubsequence(vector<int> array, vector<int> sequence) {
+  size_t arr_idx = 0;
+	size_t seq_idx = 0;
+	while (arr_idx < array.size() && seq_idx < sequence.size()) {
+		if (array[arr_idx] == sequence[seq_idx]) {
+			seq_idx++;
+		}
+		arr_idx++;
+	}
+	return seq_idx == sequnce.size();
+}
+
+// Sol-2 T=O(n), S=O(1), Range-based for loop(since C++11)
+bool isValidSubsequence(vector<int> array, vector<int> sequence) {
+	size_t seq_idx = 0;
+  for (auto value : array) {
+    if (sequence[seq_idx] == seq_idx) {
+      break;
+    }
+    if (value == sequence[seq_idx]) {
+      seq_idx++;
+    }
+  }
+	return seq_idx == sequnce.size();
+}
