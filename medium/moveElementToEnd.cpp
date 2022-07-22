f8d9e83805144a8c5104fc358fc4bd06a2c60cfc
Hyeonsong Kim <83186385+kimhxsong@users.noreply.github.com>
2022-07-23 01:38:39 +0900

    Fix

--- medium/moveElementToEnd.cpp
+++ medium/moveElementToEnd.cpp
@@ -7,11 +7,11 @@ vector<int> moveElementToEnd(vector<int> array, int toMove) {
   vector<int>::iterator cursor1 = array.begin();
   vector<int>::iterator cursor2 = array.begin();
   vector<int>::iterator last = array.end();
-  while (cursor1 != last) {
-    while (*cursor1 != toMove)
+  while (1) {
+    while (cursor1 != array.end() && *cursor1 != toMove)
       cursor1++;
     cursor2 = cursor1;
-    while (*cursor2 == toMove)
+    while (cursor2 != array.end() && *cursor2 == toMove)
       cursor2++;
     if (cursor2 == last)
       break;
