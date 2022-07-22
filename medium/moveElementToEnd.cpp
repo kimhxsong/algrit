eb0e7e62837f2a42868805ae8c8a033f023b6f09
Hyeonsong Kim <83186385+kimhxsong@users.noreply.github.com>
2022-07-23 01:02:29 +0900

    Solve MoveElementToEnd; #Array #Medium

---
+++ medium/moveElementToEnd.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+using namespace std;
+
+// 변수를 하나 줄여서 풀어볼 것
+vector<int> moveElementToEnd(vector<int> array, int toMove) {
+  vector<int>::iterator cursor1 = array.begin();
+  vector<int>::iterator cursor2 = array.begin();
+  vector<int>::iterator last = array.end();
+  while (cursor1 != last) {
+    while (*cursor1 != toMove)
+      cursor1++;
+    cursor2 = cursor1;
+    while (*cursor2 == toMove)
+      cursor2++;
+    if (cursor2 == last)
+      break;
+    swap(*cursor1, *cursor2);
+    cursor1++;
+  }
+  return array;
+}
